7d7952e91e0078d03a26c65e0253802675c34c8d
melkadze <25963683+melkadze@users.noreply.github.com>
2019-10-23 11:45:34 -0400

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+const DOMStrings = {
+    input: document.getElementById("pkmn-form"),
+    name: document.getElementById("pokemon-name"),
+    displayName: document.querySelector(".pkmn-name-size"),
+    displayImageFront: document.querySelector(".display-image-front-def"),
+    displayImageBack: document.querySelector(".display-image-back-def"),
+    displayImageShinyFront: document.querySelector(".display-image-shiny-front"),
+    displayImageShinyBack: document.querySelector(".display-image-shiny-back"),
+    displayNum: document.querySelector(".pkmn-num")
+}
+
+function getPkmn(){
+    DOMStrings.input.addEventListener('submit', async function(e){
+        e.preventDefault();
+        try{
+            const result = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${DOMStrings.name.value}`
+            );
+
+            const data = await result.json();
+
+            const displayPkmn = function(data) {
+                
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    })
+}
+
+getPkmn();
